fee08f088b535889628cb7d8af8bd45909f4d925
daniaismail <dani14sssss@gmail.com>
2023-11-15 04:21:02 +0800

    -Modified downloaded directory

--- akirakan.py
+++ akirakan.py
@@ -1,13 +1,12 @@
+import os
+
 import requests
-import logging
 from datetime import datetime, timedelta
 
-today = datetime.today()
-t = today.strftime("%d%m%y")
+today = datetime.today() - timedelta(days=1)
 ym = today.strftime("%Y-%m")
 ymd = today.strftime("%Y-%m-%d")
-
-logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%d%m%Y %I:%M:%S%p', filename=f"C:/Users/DOUBLE33/PycharmProjects/akirakan/log/log {t}.txt", level=logging.DEBUG)
+download_dir = "C:/Users/mvmwe/PycharmProjects/akirakan/files"
 locations = open('locations.txt', 'r')
 count = 0
 
@@ -20,19 +19,17 @@ while True:
     file_url = f"https://petronas:viewreport@weather.akirakan.com/archive/forecast_report/{ym}/{location}-{ymd}_issue_at_05:00_LT.pdf"
     filename = f"{location}-{ymd}_issue_at_05_00_LT.pdf"
     r = requests.get(file_url, stream=True)
+    file_path = os.path.join(download_dir,filename)
 
-    # if line is empty
-    # end of file is reached
     if not location:
         break
-    with open(f"{filename}", "wb") as pdf:
+    with open(f"{file_path}", "wb") as pdf:
         for chunk in r.iter_content(chunk_size=1024):
             # writing one chunk at a time to pdf file
             if chunk:
                 pdf.write(chunk)
 
-    print("Line{}: {}".format(count, location.strip())+file_url)
-    logging.debug('Reading line {} '.format(count, location.strip()) + file_url)
+    print(filename)
 
 locations.close()
 
