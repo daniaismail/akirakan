50051c74af67dfa51219aa389bd795a307bbd7a2
daniaismail <dani14sssss@gmail.com>
2024-05-29 07:51:00 +0800

    change link to amtmetocean.com

--- akirakan.py
+++ akirakan.py
@@ -6,7 +6,7 @@ from datetime import datetime
 today = datetime.today()
 ym = today.strftime("%Y-%m")
 ymd = today.strftime("%Y-%m-%d")
-download_dir = "C:/Users/mvmwe/PycharmProjects/akirakan/files"
+download_dir = "C:/Users/User/PycharmProjects/akirakan/files"
 locations = open('locations.txt', 'r')
 count = 0
 
@@ -16,7 +16,7 @@ while True:
     # Get next line from file
     line = locations.readline()
     location = line.rstrip('\n')
-    file_url = f"https://petronas:viewreport@weather.akirakan.com/archive/forecast_report/{ym}/{location}-{ymd}_issue_at_05:00_LT.pdf"
+    file_url = f"https://petronas:viewreport@weather.amtmetocean.com/archive/forecast_report/{ym}/{location}-{ymd}_issue_at_05:00_LT.pdf"
     filename = f"{location}-{ymd}_issue_at_05_00_LT.pdf"
     r = requests.get(file_url, stream=True)
     file_path = os.path.join(download_dir,filename)
